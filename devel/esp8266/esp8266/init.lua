3828c3f848a45ff51fa3938f875982cd09df8c06
Matthew <maverickames@gmail.com>
2016-03-16 22:49:36 -0700

    try this

--- devel/esp8266/esp8266/init.lua
+++ devel/esp8266/esp8266/init.lua
@@ -33,29 +33,29 @@ srv:listen(80,function(conn)
     conn:on("receive", function(client,request)
         local buf = "";
         local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
-        if(method == nil)then
+        if (method == nil) then
             _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
         end
         
         local _GET = {}
-        if (vars ~= nil)then
+        if (vars ~= nil) then
             for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
                 _GET[k] = v
             end 
-end
-       buf = buf.."HTTP/1.1 200 OK\r\n";--safari
-       buf = buf.."Content-type: text/html\r\n";--safari
-       buf = buf.."Connection: close\r\n\r\n";--safari
-       buf = buf.."<h1> ESP8266 Web Server</h1>";
-       buf = buf.."<p>GPIO0 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
-       buf = buf.."Temp:"..temp..".....".."humidity:"..humi;
-       buf = buf.."<meta http-equiv=refresh content=10>";
-        local _on,_off = "",""
-        if(_GET.pin == "ON1")then
-              gpio.write(led1, gpio.HIGH);
-        elseif(_GET.pin == "OFF1")then
-            gpio.write(led1, gpio.LOW);
+        end
 
+        buf = buf.."HTTP/1.1 200 OK\r\nContent-type: text/html\r\nConnection: close\r\n\r\n";--safari
+        buf = buf.."<h1><center> ESP8266 Web Server </center></h1>";
+        buf = buf.."<p>GPIO0 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
+        buf = buf.."Temp:"..temp..".....".."humidity:"..humi;
+        buf = buf.."<meta http-equiv=refresh content=10>";
+        
+        local _on,_off = "",""
+        
+        if (_GET.pin == "ON1") then
+            gpio.write(led1, gpio.HIGH);
+        elseif (_GET.pin == "OFF1") then
+            gpio.write(led1, gpio.LOW);
         end
 
         client:send(buf);
